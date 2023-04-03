e531e9c4e5e95b315952334660dd8e345229abbf
hariommahato <71360153+hariommahato@users.noreply.github.com>
2023-04-03 19:37:15 +0545

    first commit

--- Components/Carousel/Carousel.jsx
+++ Components/Carousel/Carousel.jsx
@@ -4,17 +4,9 @@ function CarouselCompo() {
   return (
     <Carousel fade>
       <Carousel.Item className={styles.carouselItem}>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/photo-1617450365226-9bf28c04e130?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-          alt="First slide"
-          style={{
-            objectFit: "cover",
-            backgroundPosition: "center",
-          }}
-        />
+        <video src={"/video.mp4"} className={styles.video} autoPlay muted />
+
         <Carousel.Caption>
-          {/* <h3>First slide label</h3>*/}
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
